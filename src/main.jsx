bb9900990652faf70494ed121a5e5d686ed81d02
GS26Surya <gsurya124@gmail.com>
2023-11-11 14:59:09 +0530

    minor change

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import AppContext from './context/contextapi.jsx';
-import React from 'react';
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <AppContext>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </AppContext>
+      <App />
   </BrowserRouter>
 );
